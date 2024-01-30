3ec86d55de630ce3529c966cfbc2412017b44534
mikygaleano <mikebostero.bernal@gmail.com>
2024-01-29 22:32:56 -0300

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = ()=> {
 
 
     return (
-        <footer className="w-full h-28 flex flex-row max-md:flex-col justify-between items-center p-2">
+        <footer className="w-full h-28 flex flex-row max-md:flex-col justify-between items-center p-2 bg-slate-300">
             <div className="m-auto">
                 <span>© 2024 Michael Galeano N. (Maikidev)</span>
             </div>
